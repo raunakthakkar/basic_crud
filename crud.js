1fda3bbabfe4467ec0669037b86180d82206c924
Raunak Thakkar <raunakthakkar4@gmail.com>
2021-01-11 23:53:32 +0530

    =inital commit with ready to publish

---
+++ crud.js
@@ -0,0 +1,41 @@
+// for reading we need some data to be present in our html
+// for updating we need some data to be present in our js file (array,object,etc)
+let items=["item1","item2","item3"];
+window.addEventListener("load",()=>{
+    mapping();
+}
+);
+
+let mapping=()=>{
+    document.getElementById("items").innerHTML=null
+    items.map((ele,i)=>{
+        let inp=document.createElement("label")
+        inp.innerHTML=ele+"<br/>"
+        inp.style.border="1 solid black";
+        document.getElementById("items").appendChild(inp)
+    })
+}
+
+let delItem=()=>{
+    if(items.length>0){
+        items.pop()
+        mapping()
+    }else{
+        alert("cannot delete items")
+    }
+}
+let addingElement=()=>{
+    if (document.getElementById('inpValue').value=="")
+     {
+         alert("cannot add blank values");
+         return false
+    }
+        else
+        {
+        let val=document.getElementById('inpValue').value
+        items.includes(val)==true?alert("ehh duplicate values"):items.push(val)
+        mapping()
+        document.getElementById('inpValue').value=null
+        }
+   
+}

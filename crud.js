fcaaab626f5e2a6c794ff1c1c71cbc6e737c7559
Raunak Thakkar <raunakthakkar4@gmail.com>
2021-01-14 12:48:09 +0530

    =commit 3 branch feature

--- crud.js
+++ crud.js
@@ -1,4 +1,3 @@
-// for reading we need some data to be present in our html
 // for updating we need some data to be present in our js file (array,object,etc)
 //for deleting we have a function delItem,for adding we have addingElement
 //items is the array we have created 
